5a35ce37c857e6334375e5afd2ef23ebfee247ae
stasia-a <stasia-a@tut.by>
2012-08-02 12:26:43 +0300

    Assertions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :date_loaded
+    
+    def date_loaded
+      return Time.now
+    end
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -16,6 +16,11 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_actions' do |elements|
+      elements.each do |element|
+        assert_select element, "a", 3
+      end
+    end
   end
 
   test "should get new" do
